f9295d3a0ac6e3526acc23ff94b921d0ec7cb80c
Adam Minchinton <adam.minchinton@gmail.com>
2025-11-15 22:03:27 +0100

    Update colour game row&column calc

--- CONVERT_ROM/source/crab_grab_game_processor.py
+++ CONVERT_ROM/source/crab_grab_game_processor.py
@@ -1,38 +1,42 @@
 # Game processor for Crab Grab
+from platform import processor
 from source.games_path_utils import GamesPathUpdater
 from source.game_processor import GameProcessor
 
 class CrabGrabGameProcessor(GameProcessor):
-    def __init__(self):
-        super().__init__()
+	def __init__(self):
+		super().__init__()
 
-        # Specifics for Crab Grab
-        self.game_key = "Crab_Grab" # Key from games_path.py
-        self.game_folder = "gnw_cgrab" # Folder name for the game
-        self.split_ratio_top = 0.4735  # Specific split ratio for Crab Grab
+		# Specifics for Crab Grab
+		self.game_key = "Crab_Grab" # Key from games_path.py
+		self.game_folder = "gnw_cgrab" # Folder name for the game
+		self.split_ratio_top = 0.4735  # Specific split ratio for Crab Grab
 
-    def post_process(self):
-        self.add_colour_indices()
+	def post_process(self):
+		self.colour_mode = "columns"
+		self.colour_names = {0: "none", 1: "purple", 2: "mint", 3: "coral", 4: "blue"}
+		self.header_fraction: float = 0.10
+		self.add_colour_indices()
 
-        # Combine the regular background with the Frame with the pink link separating the header
-        updater = GamesPathUpdater()
-        target = updater.get_target(self.game_key)
-        # Frame is in the same folder as background
-        frame_path = target.background_paths[0].parent / "Frame2.png"
-        # check frame exists
-        if not frame_path.exists():
-            print(f"Frame file not found: {frame_path} for game {self.game_key}")
-            raise SystemExit(1)
-        
-        combined_background = self.combine_backgrounds(target.background_paths[0], frame_path)
-        target.background_paths = [combined_background]
+		# Combine the regular background with the Frame with the pink link separating the header
+		updater = GamesPathUpdater()
+		target = updater.get_target(self.game_key)
+		# Frame is in the same folder as background
+		frame_path = target.background_paths[0].parent / "Frame2.png"
+		# check frame exists
+		if not frame_path.exists():
+			print(f"Frame file not found: {frame_path} for game {self.game_key}")
+			raise SystemExit(1)
+		
+		combined_background = self.combine_backgrounds(target.background_paths[0], frame_path)
+		target.background_paths = [combined_background]
 
-        # Set custom values
-        target.alpha_bright = 1.0
-        target.fond_bright = 1.3
-        target.color_segment = True
+		# Set custom values
+		target.alpha_bright = 1.0
+		target.fond_bright = 1.3
+		target.color_segment = True
 
-        updater.write()
+		updater.write()
 
-        self.multiscreen_conversion()
+		self.multiscreen_conversion()
 
--- CONVERT_ROM/source/spitball_sparky_game_processor.py
+++ CONVERT_ROM/source/spitball_sparky_game_processor.py
@@ -3,50 +3,53 @@ from source.games_path_utils import GamesPathUpdater
 from source.game_processor import GameProcessor
 
 class SpitballSparkyGameProcessor(GameProcessor):
-    def __init__(self):
-        super().__init__()
+	def __init__(self):
+		super().__init__()
 
-        # Specifics for Spitball Sparky
-        self.game_key = "Spitball_Sparky" # Key from games_path.py
-        self.game_folder = "gnw_ssparky" # Folder name for the game
-        self.split_ratio_top = 0.4884  # Specific split ratio for Spitball Sparky
+		# Specifics for Spitball Sparky
+		self.game_key = "Spitball_Sparky" # Key from games_path.py
+		self.game_folder = "gnw_ssparky" # Folder name for the game
+		self.split_ratio_top = 0.4884  # Specific split ratio for Spitball Sparky
 
-    def post_process(self):
-        self.add_colour_indices()
+	def post_process(self):
+		self.colour_mode = "rows"
+		self.colour_names = {0: "none", 1: "purple", 2: "teal", 3: "coral", 4: "blue"}
+		self.header_fraction: float = 0.15
+		self.add_colour_indices()
 
-        updater = GamesPathUpdater()
-        target = updater.get_target(self.game_key)
+		updater = GamesPathUpdater()
+		target = updater.get_target(self.game_key)
 
-        # Combine the regular background with the Frame 
-        # Frame is in the same folder as background
-        game_folder = target.background_paths[0].parent
-        frame_path = game_folder / "Frame2.png"
-        # check frame exists
-        if not frame_path.exists():
-            print(f"Frame file not found: {frame_path} for game {self.game_key}")
-            raise SystemExit(1)
-        
-        combined_background = self.combine_backgrounds(target.background_paths[0], frame_path)
-        target.background_paths = [combined_background]
+		# Combine the regular background with the Frame 
+		# Frame is in the same folder as background
+		game_folder = target.background_paths[0].parent
+		frame_path = game_folder / "Frame2.png"
+		# check frame exists
+		if not frame_path.exists():
+			print(f"Frame file not found: {frame_path} for game {self.game_key}")
+			raise SystemExit(1)
+		
+		combined_background = self.combine_backgrounds(target.background_paths[0], frame_path)
+		target.background_paths = [combined_background]
 
-        # Combine the background and Frame combo background with the Lines background
-        # Lines is in the same folder as background
-        lines_path = game_folder / "Lines2.png"
-        # check frame exists
-        if not lines_path.exists():
-            print(f"Frame file not found: {lines_path} for game {self.game_key}")
-            raise SystemExit(1)
-        
-        combined_background = self.combine_backgrounds(target.background_paths[0], lines_path)
-        target.background_paths = [combined_background]
+		# Combine the background and Frame combo background with the Lines background
+		# Lines is in the same folder as background
+		lines_path = game_folder / "Lines2.png"
+		# check frame exists
+		if not lines_path.exists():
+			print(f"Frame file not found: {lines_path} for game {self.game_key}")
+			raise SystemExit(1)
+		
+		combined_background = self.combine_backgrounds(target.background_paths[0], lines_path)
+		target.background_paths = [combined_background]
 
-        # Set custom values
-        target.alpha_bright = 1
-        target.fond_bright = 1.3
-        target.color_segment = True
+		# Set custom values
+		target.alpha_bright = 1
+		target.fond_bright = 1.3
+		target.color_segment = True
 
-        updater.write()
+		updater.write()
 
 
-        self.multiscreen_conversion()
+		self.multiscreen_conversion()
 
