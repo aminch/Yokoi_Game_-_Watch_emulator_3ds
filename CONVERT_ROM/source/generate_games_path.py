2d0bbf6726b75ba0a05b5ca8f3514f48cd64e0c2
Adam Minchinton <adam.minchinton@gmail.com>
2025-11-16 22:10:15 +0100

    Add shadow to GameEntry

--- CONVERT_ROM/source/generate_games_path.py
+++ CONVERT_ROM/source/generate_games_path.py
@@ -693,7 +693,8 @@ def generate_games_path() -> bool:
             transform_visual=transform_data,
             size_visual=size_visual_data,
             two_in_one_screen=two_in_one_screen,
-            mask=needs_mask
+            mask=needs_mask,
+            shadow=True,
         )
         entries.append(entry)
 
