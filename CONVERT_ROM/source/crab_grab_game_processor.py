ff919dd9173a5384a0c360fec518d6bffa292373
Adam Minchinton <adam.minchinton@gmail.com>
2025-11-16 22:11:00 +0100

    Update Crab Grab processor for improved look

--- CONVERT_ROM/source/crab_grab_game_processor.py
+++ CONVERT_ROM/source/crab_grab_game_processor.py
@@ -18,11 +18,33 @@ class CrabGrabGameProcessor(GameProcessor):
 		self.header_fraction: float = 0.10
 		self.add_colour_indices()
 
-		# Combine the regular background with the Frame with the pink link separating the header
+		# Combine the regular background with the Subtract, Overlay and Frame
 		updater = GamesPathUpdater()
 		target = updater.get_target(self.game_key)
+
+		# Subtract is in the same folder as background
+		game_folder = target.background_paths[0].parent
+		subtract_path = game_folder / "Subtract.png"
+		# check subtract exists
+		if not subtract_path.exists():
+			print(f"Subtract file not found: {subtract_path} for game {self.game_key}")
+			raise SystemExit(1)
+		
+		combined_background = self.combine_backgrounds(target.background_paths[0], subtract_path, mode="multiply")
+		target.background_paths = [combined_background]
+
+		# Overlay is in the same folder as background
+		overlay_path = game_folder / "Overlay.png"
+		# check overlay exists
+		if not overlay_path.exists():
+			print(f"Overlay file not found: {overlay_path} for game {self.game_key}")
+			raise SystemExit(1)
+		
+		combined_background = self.combine_backgrounds(target.background_paths[0], overlay_path, mode="add")
+		target.background_paths = [combined_background]
+
 		# Frame is in the same folder as background
-		frame_path = target.background_paths[0].parent / "Frame2.png"
+		frame_path = game_folder / "Frame2.png"
 		# check frame exists
 		if not frame_path.exists():
 			print(f"Frame file not found: {frame_path} for game {self.game_key}")
@@ -32,9 +54,10 @@ class CrabGrabGameProcessor(GameProcessor):
 		target.background_paths = [combined_background]
 
 		# Set custom values
-		target.alpha_bright = 1.0
+		target.alpha_bright = 0.8
 		target.fond_bright = 1.3
 		target.color_segment = True
+		target.shadow = False
 
 		updater.write()
 
